72aaece6450bcfdbd9fef66d028e741735bd61b8
kso12041204 <kso12041204@naver.com>
2022-02-08 18:04:23 +0900

    Day 306

--- Algorithm/Programmers/파이썬/README.md
+++ Algorithm/Programmers/파이썬/README.md
@@ -66,3 +66,51 @@ l.isdigit()
 l 
 
 ```
+
+# DFS OR BFS
+
+```
+
+
+from collections import deque
+def bfs(graph, start_node):
+    visited = []
+    need_visited = deque()
+
+    need_visited.append(start_node)
+
+    while need_visited:
+        node = need_visited.popleft()
+
+        if node not in visited:
+            visited.append(node)
+            need_visited.extend(graph[node])
+
+    return visited
+
+def dfs(graph, start, visited = []):
+    visited.append(start)
+
+    for node in graph[start]:
+        if node not in visited:
+            dfs(graph, node, visited)
+    
+    return visited
+
+graph = dict()
+ 
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+print(bfs(graph, 'A'))
+
+
+```
---
+++ 파이썬/개념/HelloWorld.py
@@ -0,0 +1,48 @@
+
+from collections import deque
+def bfs(graph, start_node):
+    visited = []
+    queue = deque(start_node)
+
+    while queue:
+        node = queue.popleft()
+
+        if node not in visited:
+            visited.append(node)
+            queue.extend(graph[node])
+
+    return visited
+
+def dfs(graph, start, visited = []):
+    visited.append(start)
+
+    for node in graph[start]:
+        if node not in visited:
+            dfs(graph, node, visited)
+    
+    return visited
+
+
+def dfs(graph, start, visited = []):
+    visited.append(start)
+
+    for node in graph[start]:
+        if node not in visited:
+            dfs(graph, node, visited)
+
+    return visited
+
+graph = dict()
+ 
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+print(bfs(graph, 'A'))
